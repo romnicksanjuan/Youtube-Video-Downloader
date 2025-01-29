64648d712a1069b2520fa113d8715155fc5bd0b8
romnick <romnicksanjuan9@gmail.com>
2025-01-29 12:05:10 +0800

    2nd

--- server/server.js
+++ server/server.js
@@ -145,7 +145,7 @@ app.get("/download", async (req, res) => {
         });
     } catch (error) {
         console.error(error);
-        res.status(500).send("An error occurred while processing the request");
+        res.status(500).send("An error occurred while processing the request" + error);
     }
 
 });
